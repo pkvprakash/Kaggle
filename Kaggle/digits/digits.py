953eaee6935a0c2633a508394e4bd8902b1d9154
Vishnuprakash Puthiya Kovilakath <pkvprakash@gmail.com>
2015-05-16 12:27:59 +0530

    Initial version of Digit Identifier

---
+++ Kaggle/digits/digits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+train = pd.read_csv('./input/train.csv')
+test  = pd.read_csv('./input/test.csv')
+
+y = train[['label']]
+x = train.drop('label', axis=1)
+
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(x,np.ravel(y))
+pred = clf.predict(test)
+
+submission = pd.DataFrame({"ImageId":test.index, "Label":pred})
+submission.to_csv('rf_python_digits.csv', index=False)
